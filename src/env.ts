72153a6227ae19dbaf34c06b2a54a2b87b0af03e
Long Pham <phamlong0711@gmail.com>
2019-11-27 18:36:54 +0700

    Update mongodb uri

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,3 @@
 export default {
-  MONGODB_URI: process.env.NODE_ENV === 'production' ? 'mongodb://localhost/vws' : process.env.MONGODB_URI
+  MONGODB_URI: process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://localhost/vws'
 }
